4540fa6cd58ff55753b2e25ac55e540640188613
Reyes-Code <reyespiraligua@gmail.com>
2025-05-20 00:29:26 -0400

    v1.01

--- src/private.py
+++ src/private.py
@@ -1,6 +1,6 @@
 class PrivateKey:
     def __init__(self):
         self.__private_key = "2Tf,gSQk2(58rQX$1R*R:z+k*e;HX!" 
-
+        self.__private_keysecond = "aaa123" 
     def get_private_key(self):
-        return self.__private_key
+        return self.__private_key,  self.__private_keysecond 
